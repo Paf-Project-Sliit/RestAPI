69aa526355b48b38e120abc6faf5d05cd13c6852
DELL <DELL@DESKTOP-CCVJ4QF>
2021-04-21 11:14:06 +0530

    Research Part added (jersey)

--- RestAPI/src/main/java/com/gadgetbadget/restapi/Entity/Researcher.java
+++ RestAPI/src/main/java/com/gadgetbadget/restapi/Entity/Researcher.java
@@ -18,7 +18,55 @@ public class Researcher {
     @Id
     @GeneratedValue
     private int Id;
-    private String FirstName;
+    public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getPhoneNo() {
+		return PhoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		PhoneNo = phoneNo;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getLinkedIn() {
+		return LinkedIn;
+	}
+	public void setLinkedIn(String linkedIn) {
+		LinkedIn = linkedIn;
+	}
+	public String getUniversity() {
+		return University;
+	}
+	public void setUniversity(String university) {
+		University = university;
+	}
+	private String FirstName;
     private String LastName;
     private String Address;
     private String PhoneNo;
